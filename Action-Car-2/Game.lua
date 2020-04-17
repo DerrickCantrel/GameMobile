020b3b1be795967b88ef5778f0c8233f205b181d
DerrickCantrel <derrickcantrell16@gmail.com>
2020-04-17 18:49:36 -0300

    game start + end

--- Action-Car-2/Game.lua
+++ Action-Car-2/Game.lua
@@ -10,21 +10,37 @@ composer = require( "composer" )
 cenag = composer.newScene()
 
 -- VARIAVEIS
-
-
+Velocidade = 5
+C1W = 569 + display.contentWidth/2
 
 -- CRIANDO A CENA
-
 function cenag:create(event)
 
     local grupoCena = self.view
 
+    -- TELA REPEAT
+
+    mapa = display.newImageRect("imgs/gamemap.png", 600, 2550 )
+    mapa.x = display.contentCenterX
+    mapa.y = - 5680
+
+    grupoCena:insert(gamemap)
+
+    mapacopy = display.newImageRect("imgs/gamemap.png", 600, 2550 )
+    mapacopy.x = display.contentCenterX
+    mapacopy.y = - 5680
+
+    grupoCena:insert(mapacopy)
+
+    --[[
     -- TESTE RETOMADA DE TELA
     gamemap2 = display.newImageRect("imgs/gamemap2.png", 600, 12000 )
     gamemap2.x = display.contentCenterX
     gamemap2.y = - 5680
+    
 
     grupoCena.insert(gamemap2)
+    ]]--
 
     -- Limites da via (onCollide)
     limiteLateral1 = display.newRect(100, 160, 20, 320)
@@ -41,6 +57,7 @@ function cenag:create(event)
 
     grupoCena:insert(limiteLateral2)
 
+    --[[
     local function onLocalCollision( self, event )
 
         if ( event.phase == "began" ) then
@@ -54,8 +71,10 @@ function cenag:create(event)
     local function movbackground( event )
         gamemap2.y = gamemap2.y + 2
     end
+    
 
     timerBackground = timer.performWithDelay( 10, movbackground, 0)
+    ]]--
 
     veiculo = display.newImageRect("imgs/Motocicleta.png", 40, 90 )
     veiculo.x = display.contentCenterX
@@ -63,6 +82,10 @@ function cenag:create(event)
     veiculo.rotation = 0
     veiculo.myName = "Veiculo"
     physics.addBody( veiculo, "dynamic", {radius = 30, bounce=0.3} )
+
+    p:play()
+
+    grupoCena:insert(veiculo)
 
     -- BOTÕES QUE CONTROLAM A MOTO
     local botao = {}
@@ -94,54 +117,109 @@ function cenag:create(event)
     local moveX = 0
     local moveY = 0
 
-    local function funcaoToque(e)
-        if e.phase == "began" or e.phase == "moved" then
-            if e.target.myName == "cima" then
-                moveY = -5
-                moveX = 0
-            elseif  e.target.myName == "baixo" then
-                moveY = 5
-                moveX = 0
-            elseif  e.target.myName == "esquerda" then
-                moveY = 0
-                moveX = -5
-            elseif  e.target.myName == "direita" then
-                moveY = 0
-                moveX = 5
-            end
-        else 
-            moveX = 0
-            moveY = 0
-        end
-    end
+    grupoCena:insert(botao[1])
+    grupoCena:insert(botao[2])
+    grupoCena:insert(botao[3])
+    grupoCena:insert(botao[4])
 
-    local j=1
 
-    for j=1, 4, 1 do
-        botao[j]:addEventListener( "touch", funcaoToque )
-    end
+end
 
-    local function update()
-        veiculo.x = veiculo.x + moveX
-        veiculo.y = veiculo.y + moveY
-    end
+--####################################################################
+--Fun��es auxiliares
+--####################################################################
+--####################################################################
+-- ATUALIZAR O A POSICAO DO VEICULO
+function update()
+    veiculo.x = veiculo.x + moveX
+    veiculo.y = veiculo.y + moveY
+end
 
-    Runtime:addEventListener("enterFrame", update)
-
-    function delimitarVeiculo()
-        veiculo.rotation = 0
-        veiculo.x = 400
-        veiculo.y = 200
-    end
-
-    -- Transição para a cena reiniciar
-    --[[
-    function GamerOver()
+-- função da tela repetindo
+function Move_mapaScrollPai()    
+ 
+     
+    if(mapa.x - display.contentWidth < - C1W )then
+         
+         mapa.x = mapa.width+mapacopy.x - Velocidade
+         
+         Ciclos = Ciclos + 1
+         
+         --print(Ciclos)
+         
+        else
+         
+            mapa.x = mapa.x - Velocidade
         
-        storyboard.gotoScene("Reiniciar", "fade", 800)
-
+        end  
+     
     end
-    ]]--
+   
+   
+    function Move_mapaScrollFilho()  
+      
+      
+        if(mapacopy.x - display.contentWidth < - C1W )then
+            
+            mapacopy.x = mapacopy.width+mapa.x
+    
+            else
+            
+                mapacopy.x = mapacopy.x - Velocidade  
+    
+            end
+      
+    end
+   
+end
+
+function funcaoToque(e)
+    if e.phase == "began" or e.phase == "moved" then
+        if e.target.myName == "cima" then
+            moveY = -5
+            moveX = 0
+        elseif  e.target.myName == "baixo" then
+            moveY = 5
+            moveX = 0
+        elseif  e.target.myName == "esquerda" then
+            moveY = 0
+            moveX = -5
+        elseif  e.target.myName == "direita" then
+            moveY = 0
+            moveX = 5
+        end
+    else 
+        moveX = 0
+        moveY = 0
+    end
+end
+
+local j=1
+
+for j=1, 4, 1 do
+    botao[j]:addEventListener( "touch", funcaoToque )
+end
+
+Runtime:addEventListener("enterFrame", update)
+
+function cenag:show(event)
+
+    runtime:addEventListener("enterFrame", update)
+    Runtime:addEventListener("enterFrame",Move_mapaScrollPai)
+    Runtime:addEventListener("enterFrame",Move_mapaScrollFilho)
+
+end
+
+function cenag:hide(event)
+
+    Runtime:removeEventListener("enterFrame",update)
+    Runtime:removeEventListener("enterFrame",Move_mapaScrollPai)
+    Runtime:removeEventListener("enterFrame",Move_mapaScrollFilho)
+  
+end
+
+function cenag:destroy(event)
+ 
 end
 
 cenag:addEventListener("create",cenai)
