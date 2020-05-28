590941ed76b5fc735a46446a502f06699359f74a
DerrickCantrel <derrickcantrell16@gmail.com>
2020-05-28 20:10:41 -0300

    is complete

--- Action-Car-2/Resume.lua
+++ Action-Car-2/Resume.lua
@@ -1,58 +1,42 @@
 local composer = require( "composer" )
 local cenar = composer.newScene()
 
-scoreShow = 0 
-
 function cenar:create(event)
 
     local grupoR = self.view
 
-    background = display.newImageRect("imgs/fim.jpg",800,480)
+    background = display.newImage("imgs/fim.jpg")
     background.x = display.contentWidth/2
     background.y = display.contentHeight/2
+    background:scale(0.7, 0.7)
 
     grupoR:insert(background)
 
-    tenteNovamente = display.newImageRect("imgs/gamer over 2.png",200,50)
-    tenteNovamente.x = display.contentWidth/2
-    tenteNovamente.y = display.contentHeight/2
-    tenteNovamente.myName = "Novamente"
+    tenteNovamente = display.newImage("imgs/tentarNovamente.png")
+    tenteNovamente.x = display.contentWidth/2; tenteNovamente.y = display.contentHeight/2
+    tenteNovamente:scale(0.6, 0.6)
 
     grupoR:insert(tenteNovamente)
 
-    Titulo = display.newText("SCORE",display.contentWidth/2,display.contentHeight/10,native.systemFontBold,30)
-	Titulo:setFillColor(0,0,0)
-	
-    grupoR:insert(Titulo)
-    
-    --Texto = display.newText(scoreShow,display.contentWidth/3.8,display.contentHeight/3,"Arial",40)    
-	--Texto:setFillColor(0,0,0)
-    --grupoR:insert(Texto)
-  
+    --sound
 end
 
-function novamente()
-
-    composer.gotoScene("Game", 800)
-end
-
-function tentarNovamente(event)
-    if ( "ended" == event.phase ) then
-      
-            
+function restart(event)
+    if event.phase == "began" then
+        composer.gotoScene("Game", "fade", 400)
     end
 end
 
 function cenar:show(event)
 
-composer.removeScene( "Game", true )
-Runtime:addEventListener("touch",tentarNovamente)
+    composer.removeHidden()
+    tenteNovamente:addEventListener("touch", restart)
     
 end
     
 function cenar:hide(event)
     
-Runtime:removeEventListener("touch",tentarNovamente)
+    tenteNovamente:removeEventListener("touch", restart)
         
 end
     
